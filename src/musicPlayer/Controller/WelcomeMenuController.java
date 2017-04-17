d0143156665fa8959bc1291ee0fa69ab1154fbab
Federica <federica.comuni0002@stud.hkr.se>
2017-04-17 09:57:36 +0200

    mp3 Player addede metadata reading, loop button and volume slider

--- src/musicPlayer/Controller/WelcomeMenuController.java
+++ src/musicPlayer/Controller/WelcomeMenuController.java
@@ -4,14 +4,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.mp3.Mp3Parser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,12 +37,14 @@ public class WelcomeMenuController implements Initializable {
     @FXML private Label lblTrackName;
     @FXML private Label lblElapsedTime;
     @FXML private Label lblTimeLeft;
+    @FXML private Label lblTrackArtist;
     @FXML private Slider sliderPlay;
+    @FXML private Slider sliderVolume;
+    @FXML private ToggleButton tglLoop;
+    @FXML private ImageView imgVolume;
     private Media media;
     private MediaPlayer mediaPlayer;
     private MediaView mediaView;
-    Duration currentTime;
-    Duration duration;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -41,14 +54,53 @@ public class WelcomeMenuController implements Initializable {
         btnStop.setFill(new ImagePattern(img1));
         Image img2 = new Image("PauseNormal.jpg");
         btnPause.setFill(new ImagePattern(img2));
-        Path path = Paths.get("ace of base - all that she wants.mp3");
+        Path path = Paths.get("Rage Against the Machine - Evil Empire [02] Bulls On Parade.mp3");
+        tglLoop.setText("⟳");
+        imgVolume.setImage(new Image("VolumeHigh.png"));
+        Metadata metadata = new Metadata();
+
+        //reads mp3 file's metadata
+        try {
+            InputStream input = new FileInputStream(new File("Rage Against the Machine - Evil Empire [02] Bulls On Parade.mp3"));
+            ContentHandler handler = new DefaultHandler();
+            Parser parser = new Mp3Parser();
+            ParseContext parseCtx = new ParseContext();
+            parser.parse(input, handler, metadata, parseCtx);
+            input.close();
+        } catch (Exception fe) {
+            fe.printStackTrace();
+        }
 
 
         media = new Media(path.toUri().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaView = new MediaView(mediaPlayer);
+        sliderVolume.setValue(mediaPlayer.getVolume() * 100);
+
+        sliderVolume.valueProperty().addListener(observable -> {
+            mediaPlayer.setVolume(sliderVolume.getValue() / 100);
+            if (sliderVolume.getValue() == 0.0) {
+                imgVolume.setImage(new Image("VolumeOff.png"));
+            } else if (sliderVolume.getValue() > 0.0 && sliderVolume.getValue() < 40) {
+                imgVolume.setImage(new Image("VolumeLow.png"));
+            } else if (sliderVolume.getValue() >= 40 && sliderVolume.getValue() < 75) {
+                imgVolume.setImage(new Image("VolumeMed.png"));
+            } else {
+                imgVolume.setImage(new Image("VolumeHigh.png"));
+            }
+        });
+
         lblElapsedTime.setText("00:00");
         lblTimeLeft.setText(formatTime(mediaPlayer.getTotalDuration().toSeconds()));
+
+        if (metadata.get("title") != null && metadata.get("xmpDM:artist") != null) {
+            lblTrackName.setText(metadata.get("title")); //displays metadata
+            lblTrackArtist.setText(metadata.get("xmpDM:artist"));
+        } else {
+            String fileName = path.getFileName().toString();
+            fileName = fileName.substring(0, fileName.length() - 4); //removes ".mp3" from file name
+            lblTrackName.setText(fileName);
+        }
 
 
         mediaPlayer.setOnReady(new Runnable() {
@@ -65,8 +117,10 @@ public class WelcomeMenuController implements Initializable {
                         lblElapsedTime.setText(formatTime(mediaPlayer.getCurrentTime().toSeconds()));
                         lblTimeLeft.setText(formatTime(mediaPlayer.getTotalDuration().toSeconds() - mediaPlayer.getCurrentTime().toSeconds()));
 
-                       if (mediaPlayer.getTotalDuration().toSeconds() - mediaPlayer.getCurrentTime().toSeconds() < 0.0) {
+                       if (mediaPlayer.getTotalDuration().toSeconds() - mediaPlayer.getCurrentTime().toSeconds() < 0.0 && !tglLoop.isSelected()) {
                            mediaPlayer.stop();
+                       } else if (tglLoop.isSelected()) {
+                           mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
                        }
                     }
                 );
@@ -172,14 +226,23 @@ public class WelcomeMenuController implements Initializable {
 
     private String formatTime(double time) {
 
-        int numOfMinutes = 0;
-        int numOfSeconds = 0;
+        int numOfMinutes;
+        int numOfSeconds;
+        int numOfHours;
 
-        double temp = 0;
+        if (time > 3600) { //longer than 1 hour
+            numOfSeconds = (int)(time % 60);
+            time /= 60;
+            numOfMinutes = (int)(time % 60);
+            numOfHours = (int)(time / 60);
+            return String.format("%02d:%02d:%02d", numOfHours, numOfMinutes, numOfSeconds);
+        } else {
+            numOfSeconds = (int)(time % 60);
+            numOfMinutes = (int)(time / 60);
+            return String.format("%02d:%02d", numOfMinutes, numOfSeconds);
+        }
 
-        numOfSeconds = (int)(time % 60);
-        numOfMinutes = (int)(time / 60);
-        return String.format("%02d:%02d", numOfMinutes, numOfSeconds);
+
     }
 }
 
