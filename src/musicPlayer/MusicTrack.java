2e78140e1c858567710d2ccc4c25ca432f0d831d
Viktor <Viktor@Viktor-PC>
2017-04-20 09:49:03 +0200

    Fixed previous commit with an error.

--- src/musicPlayer/MusicTrack.java
+++ src/musicPlayer/MusicTrack.java
@@ -7,37 +7,37 @@ import java.time.Duration;
  */
 public class MusicTrack {
 
-    private String trackName;
-    private Genre musicGenre;
-    private Duration time;
+	private String trackName;
+	private Genre musicGenre;
+	private Duration time;
 
-    public MusicTrack(String trackName, Genre musicGenre, Duration time) {
-        this.trackName = trackName;
-        this.musicGenre = musicGenre;
-        this.time = time;
-    }
+	public MusicTrack(String trackName, String string, musicPlayer.model.Genre classical) {
+		this.trackName = trackName;
+		this.musicGenre = string;
+		this.time = classical;
+	}
 
-    public String getTrackName() {
-        return trackName;
-    }
+	public String getTrackName() {
+		return trackName;
+	}
 
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
+	public void setTrackName(String trackName) {
+		this.trackName = trackName;
+	}
 
-    public Genre getMusicGenre() {
-        return musicGenre;
-    }
+	public Genre getMusicGenre() {
+		return musicGenre;
+	}
 
-    public void setMusicGenre(Genre musicGenre) {
-        this.musicGenre = musicGenre;
-    }
+	public void setMusicGenre(Genre musicGenre) {
+		this.musicGenre = musicGenre;
+	}
 
-    public Duration getTime() {
-        return time;
-    }
+	public Duration getTime() {
+		return time;
+	}
 
-    public void setTime(Duration time) {
-        this.time = time;
-    }
+	public void setTime(Duration time) {
+		this.time = time;
+	}
 }
