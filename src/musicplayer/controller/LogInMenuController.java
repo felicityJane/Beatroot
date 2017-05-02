c7553891d2b284db7f70147904c02b9fdc5f3fc1
felicityJane <felicity.thompson@hotmail.com>
2017-05-02 16:53:22 +0200

    fix error in sceneBuilder

--- src/musicplayer/controller/LogInMenuController.java
+++ src/musicplayer/controller/LogInMenuController.java
@@ -16,12 +16,12 @@ public class LogInMenuController implements Initializable{
     @FXML private TextField userName;
     @FXML private TextField userPassword;
     @FXML private MainMenuController mainMenuController;
-    @FXML private AnchorPane logInAnchorPane;
+    @FXML private AnchorPane logInParentAnchorPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mainMenuController.init(this);
-        mainMenuController.menuBarFitToParent(logInAnchorPane);
+        mainMenuController.menuBarFitToParent(logInParentAnchorPane);
     }
     @FXML
     private void handleLoginButton(ActionEvent event) {
