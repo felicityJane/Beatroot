beb389b7a2183e49a188ccaeca96eb9870f36186
fatsay <fatih_sayilir@hotmail.com>
2017-05-04 12:50:07 +0200

    fix

--- src/musicplayer/Main.java
+++ src/musicplayer/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
         @Override
         public void start(Stage primaryStage) throws Exception{
             //always begin in the logInMenu scene
-            Parent root = FXMLLoader.load(getClass().getResource("view/welcomeMenu.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("view/signUpMenu.fxml"));
             primaryStage.setTitle("");
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
