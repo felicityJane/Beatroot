3e203029d84632f4fa841c314e2f7f576095633c
Viktor <Viktor@Viktor-PC>
2017-05-15 07:58:32 +0200

    Working tree cleanup

--- src/musicplayer/Main.java
+++ src/musicplayer/Main.java
@@ -1,34 +1,32 @@
 package musicplayer;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import musicplayer.model.MusicArtist;
-
-import java.net.URL;
 
 public class Main extends Application {
 
-        @Override
-        public void start(Stage primaryStage) throws Exception{
-            //Application.setUserAgentStylesheet(getClass().getResource("css/styleSheet.css").toExternalForm());
-            Parent root = FXMLLoader.load(getClass().getResource("view/welcomeMenu.fxml"));
-            primaryStage.setTitle("Beatroot");
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Application.setUserAgentStylesheet(getClass().getResource("css/styleSheet.css").toExternalForm());
+		Parent root = FXMLLoader.load(getClass().getResource("view/AdminLogIn.fxml"));
+		primaryStage.setTitle("Beatroot");
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
 
-            URL url = this.getClass().getResource("css/styleSheet.css");
-            if (url==null){
-                System.out.println("Not found");
-                System.exit(0);
-            }
-            String css = url.toExternalForm();
-            scene.getStylesheets().add(css);
-            primaryStage.show();
-        }
+		URL url = this.getClass().getResource("css/styleSheet.css");
+		if (url == null) {
+			System.out.println("Not found");
+			System.exit(0);
+		}
+		String css = url.toExternalForm();
+		scene.getStylesheets().add(css);
+		primaryStage.show();
+	}
 
 	public static void main(String[] args) {
 		launch(args);
