513b03845457425b5dd84db4156bbc5af36fba64
felicityJane <felicity.thompson@hotmail.com>
2017-05-15 13:12:55 +0200

    more changes to song, album and artist pages

--- src/musicplayer/controller/AlbumPageController.java
+++ src/musicplayer/controller/AlbumPageController.java
@@ -1,22 +1,27 @@
 package musicplayer.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import musicplayer.DB_Connector;
+import musicplayer.SceneManager;
 import musicplayer.Server_Connector;
 import musicplayer.model.Album;
 import musicplayer.model.GlobalVariables;
 import musicplayer.model.MusicArtist;
 import musicplayer.model.MusicTrack;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
 
 public class AlbumPageController implements Initializable {
     @FXML private AnchorPane albumPageAnchorPane;
@@ -35,6 +40,7 @@ public class AlbumPageController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         globalVariables.setAlbumPageController(this);
         getAlbumInfo();
+        popUpMenuAlbumPage(imageView);
     }
 
     public void getAlbumInfo(){
@@ -55,5 +61,37 @@ public class AlbumPageController implements Initializable {
         }
         DropShadow dropShadow = new DropShadow(10, 0, 0, Color.GRAY);
         imageView.setEffect(dropShadow);
+    }
+    private void popUpMenuAlbumPage(ImageView imageView){
+        final ContextMenu contextMenu = new ContextMenu();
+        final MenuItem songPage = new MenuItem("See song info");
+        final MenuItem artistPage = new MenuItem("See artist info");
+        contextMenu.getItems().addAll(songPage,artistPage);
+        SceneManager sceneManager = new SceneManager();
+        for (Node n : albumPageAnchorPane.getChildren()) {
+
+            if (n instanceof ImageView ) {
+                n.setOnContextMenuRequested(event -> contextMenu.show(n, event.getScreenX(), event.getScreenY()));
+            }
+        }
+        songPage.setOnAction(event -> {
+            try {
+                Stage stage = (Stage) albumPageAnchorPane.getScene().getWindow();
+                stage.close();
+                sceneManager.popUpWindow(event, "view/songPage.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        artistPage.setOnAction(event -> {
+            try {
+                Stage stage = (Stage) albumPageAnchorPane.getScene().getWindow();
+                stage.close();
+                sceneManager.popUpWindow(event, "view/artistPage.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
 }
